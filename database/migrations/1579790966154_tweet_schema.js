f375be85c185a528392a7fd6f34eb90e8f5a5f3c
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-23 12:01:27 -0300

    Edit migration tweet

--- database/migrations/1579790966154_tweet_schema.js
+++ database/migrations/1579790966154_tweet_schema.js
@@ -7,6 +7,17 @@ class TweetSchema extends Schema {
   up () {
     this.create('tweets', (table) => {
       table.increments()
+
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+      table.string('content', 240).notNullable();
       table.timestamps()
     })
   }
