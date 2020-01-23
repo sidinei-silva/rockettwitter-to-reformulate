dce20717c64ef4688899bde01b3f87fb51c370fc
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-23 11:14:15 -0300

    Create controller with method register

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const User = use('App/Model/User')
+
+class AuthController {
+  async register({request}){
+    const data = request.only([ "username", "email", "password"]);
+
+    const user = await User.create(data);
+
+    return user;
+  }
+
+  async authenticate(){
+
+  }
+}
+
+module.exports = AuthController
